a3bd8cc7503b0c969ba218fecb6e81d054b61cac
Tenvence <tenvence@qq.com>
2025-12-13 22:02:28 +0800

    card_conference

--- js/card_papers.js
+++ js/card_papers.js
@@ -1,13 +1,9 @@
-function createPreprintItem(title, authors, equalContributor, correspondence) {
-    const peperLi = document.createElement('li');
-    peperLi.innerHTML = `<a class="paper-title">${title}</a>`
-    peperLi.innerHTML += `<br>`
-
+function getAuthorsStr(authors, equalContributor, correspondence) {
     authors = authors.split(';').map(s => s.trim());
     equalContributor = equalContributor ? equalContributor.split(';').map(s => s.trim()) : [];
     correspondence = correspondence ? correspondence.split(';').map(s => s.trim()) : [];
 
-    peperLi.innerHTML += authors.map(author => {
+    const authorsStr = authors.map(author => {
         let formatted = author;
         if (equalContributor.includes(author)) {
             formatted += '*';
@@ -17,8 +13,30 @@ function createPreprintItem(title, authors, equalContributor, correspondence) {
         }
         return formatted;
     }).join(', ');
+    return authorsStr;
+}
 
-    return peperLi;
+function createPreprintItem(title, authors, equalContributor, correspondence) {
+    const paperLi = document.createElement('li');
+    paperLi.innerHTML = `<a class="paper-title">${title}</a>`;
+    paperLi.innerHTML += `<br>`;
+    paperLi.innerHTML += getAuthorsStr(authors, equalContributor, correspondence);
+    return paperLi;
+}
+
+function createConferenceItem(conferenceId, conferenceYear, title, authors, equalContributor, correspondence, pubName, pubLevel) {
+    const paperLi = document.createElement('li');
+    paperLi.innerHTML = `<a class="pub-badge">[${conferenceId}-${conferenceYear}]</a> `;
+    paperLi.innerHTML += `<a class="paper-title">${title}</a>`;
+    paperLi.innerHTML += `<br>`;
+    paperLi.innerHTML += getAuthorsStr(authors, equalContributor, correspondence);
+    paperLi.innerHTML += `<br>`;
+    if (pubLevel) {
+        paperLi.innerHTML += `<a class="pub-info">${pubName} (<b class="pub-id">${conferenceId}</b>, <b class="pub-level">${pubLevel}</b>), ${conferenceYear}</a>`;
+    } else {
+        paperLi.innerHTML += `<a class="pub-info">${pubName} (<b class="pub-id">${conferenceId}</b>), ${conferenceYear}</a>`;
+    }
+    return paperLi;
 }
 function addPreprints(paperData) {
     const paperList = document.querySelector('#card_preprints .paper-list');
@@ -35,125 +53,22 @@ function addPreprints(paperData) {
     });
 }
 
-function separateStudents(peopleData) {
-    const graduatedPhd = [];
-    const graduatedMs = [];
-    const currentPhd = [];
-    const currentMs = [];
+function addConference(paperData, pubInfo) {
+    const paperList = document.querySelector('#card_conference .paper-list');
+    paperList.innerHTML = '';
 
-    peopleData.forEach(person => {
-        if (person.GRADUATION_YEAR && person.GRADUATION_YEAR !== '') {
-            if (person.DEGREE === 'D') {
-                graduatedPhd.push(person);
-            } else if (person.DEGREE === 'M') {
-                graduatedMs.push(person);
-            }
-        } else {
-            if (person.DEGREE === 'D') {
-                currentPhd.push(person);
-            } else if (person.DEGREE === 'M') {
-                currentMs.push(person);
-            }
-        }
-    });
-    return [graduatedPhd, graduatedMs, currentPhd, currentMs];
-}
-
-function addCurrentPhdStudents(studentData) {
-    const studentDict = {};
-    for (let i = 0; i < studentData.length; i++) {
-        if (!studentDict[studentData[i].ENROLLMENT]) {
-            studentDict[studentData[i].ENROLLMENT] = [];
-        }
-        studentDict[studentData[i].ENROLLMENT].push({
-            NAME: studentData[i].NAME,
-            HOMEPAGE_URL: studentData[i].HOMEPAGE_URL,
-            CLASS: studentData[i].CLASS
-        });
-    }
-
-    const studentList = document.querySelector('#card_phd_students .card-content');
-    studentList.innerHTML = '';
-
-    Object.keys(studentDict).sort().forEach(year => {
-        const a = document.createElement('a');
-        a.className = 'grade-separator';
-        a.innerHTML = `${year} 级`
-        studentList.appendChild(a);
-
-        const div = document.createElement('div');
-        div.className = 'grid-card-content';
-        studentDict[year].forEach(std => {
-            p = document.createElement('p');
-            p.innerHTML = std.HOMEPAGE_URL ? `<a class="people-name" href="${std.HOMEPAGE_URL}" target="_blank">${std.NAME}</a>` : `<a class="people-name">${std.NAME}</a>`;
-            if (std.CLASS !== null) {
-                p.innerHTML += `<br> <a class="people-title">（${std.CLASS === 0 ? '硕博连读' : '直博'}）</a>`;
-            }
-            div.appendChild(p)
-        });
-        studentList.appendChild(div);
-    })
-}
-
-function addCurrentMsStudents(studentData) {
-    const studentDict = {};
-    for (let i = 0; i < studentData.length; i++) {
-        if (!studentDict[studentData[i].ENROLLMENT]) {
-            studentDict[studentData[i].ENROLLMENT] = [];
-        }
-        studentDict[studentData[i].ENROLLMENT].push({
-            NAME: studentData[i].NAME,
-            HOMEPAGE_URL: studentData[i].HOMEPAGE_URL
-        });
-    }
-
-    const studentList = document.querySelector('#card_ms_students .card-content');
-    studentList.innerHTML = '';
-
-    Object.keys(studentDict).sort().forEach(year => {
-        const a = document.createElement('a');
-        a.className = 'grade-separator';
-        a.innerHTML = `${year} 级`
-        studentList.appendChild(a);
-
-        const div = document.createElement('div');
-        div.className = 'grid-card-content';
-        studentDict[year].forEach(std => {
-            p = document.createElement('p');
-            p.innerHTML = std.HOMEPAGE_URL ? `<a class="people-name" href="${std.HOMEPAGE_URL}" target="_blank">${std.NAME}</a>` : `<a class="people-name">${std.NAME}</a>`;
-            div.appendChild(p)
-        });
-        studentList.appendChild(div);
-    })
-}
-
-function addGraduatedPhdStudents(studentData) {
-    const studentList = document.querySelector('#grad-phd-content');
-    studentList.innerHTML = '';
-
-    studentData.reverse().forEach(std => {
-        p = document.createElement('p');
-        p.innerHTML = std.HOMEPAGE_URL ? `<a class="people-name" href="${std.HOMEPAGE_URL}" target="_blank">${std.NAME}</a>` : `<a class="people-name">${std.NAME}</a>`;
-        p.innerHTML += '<br>';
-        p.innerHTML += `<a>${std.GRADUATION_YEAR}</a>`;
-        p.innerHTML += '<br>';
-        p.innerHTML += `<a class="people-title">（${std.DESTINATION}）</a>`
-        studentList.appendChild(p);
-    });
-}
-
-function addGraduatedMsStudents(studentData) {
-    const studentList = document.querySelector('#grad-ms-content');
-    studentList.innerHTML = '';
-
-    studentData.reverse().forEach(std => {
-        p = document.createElement('p');
-        p.innerHTML = std.HOMEPAGE_URL ? `<a class="people-name" href="${std.HOMEPAGE_URL}" target="_blank">${std.NAME}</a>` : `<a class="people-name">${std.NAME}</a>`;
-        p.innerHTML += '<br>';
-        p.innerHTML += `<a>${std.GRADUATION_YEAR}</a>`;
-        p.innerHTML += '<br>';
-        p.innerHTML += `<a class="people-title">（${std.DESTINATION}）</a>`
-        studentList.appendChild(p);
+    paperData.forEach(paper => {
+        const paperItem = createConferenceItem(
+            paper.CONFERENCE_ID,
+            paper.CONFERENCE_YEAR,
+            paper.TITLE,
+            paper.AUTHORS,
+            paper.EQUAL_CONTRIBUTION,
+            paper.CORRESPONDENCE,
+            pubInfo[paper.CONFERENCE_ID].NAME,
+            pubInfo[paper.CONFERENCE_ID].LEVEL
+        );
+        paperList.appendChild(paperItem);
     });
 }
 
@@ -167,13 +82,12 @@ function addGraduatedMsStudents(studentData) {
     ];
 
     const allPapers = await loadCSVs(paper_files);
-    addPreprints(allPapers[0]);
-    // console.log(allPapers);
-    // addMultipleTeachers(allPeople[0]);
+    const pubInfo = await loadCSVs(['databases/publications/pub_info.csv']);
+    const pubInfoDict = pubInfo[0].reduce((acc, item) => {
+        acc[item.ID] = item;
+        return acc;
+    }, {});
 
-    // const separatedStudents = separateStudents(allPeople[1]);
-    // addGraduatedPhdStudents(separatedStudents[0]);
-    // addGraduatedMsStudents(separatedStudents[1]);
-    // addCurrentPhdStudents(separatedStudents[2]);
-    // addCurrentMsStudents(separatedStudents[3]);
+    addPreprints(allPapers[0].reverse());
+    addConference(allPapers[1].reverse(), pubInfoDict);
 })();
