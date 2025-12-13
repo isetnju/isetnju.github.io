cd41ae79560335a5b48e9f5eaf1b3a5a5d346991
Tenvence <tenvence@qq.com>
2025-12-13 22:11:09 +0800

    card_journals

--- js/card_papers.js
+++ js/card_papers.js
@@ -38,6 +38,22 @@ function createConferenceItem(conferenceId, conferenceYear, title, authors, equa
     }
     return paperLi;
 }
+
+function createJournalItem(journalId, title, authors, equalContributor, correspondence, pubName, pubLevel) {
+    const paperLi = document.createElement('li');
+    paperLi.innerHTML = `<a class="pub-badge">[${journalId}]</a> `;
+    paperLi.innerHTML += `<a class="paper-title">${title}</a>`;
+    paperLi.innerHTML += `<br>`;
+    paperLi.innerHTML += getAuthorsStr(authors, equalContributor, correspondence);
+    paperLi.innerHTML += `<br>`;
+    if (pubLevel) {
+        paperLi.innerHTML += `<a class="pub-info">${pubName} (<b class="pub-id">${journalId}</b>, <b class="pub-level">${pubLevel}</b>)</a>`;
+    } else {
+        paperLi.innerHTML += `<a class="pub-info">${pubName} (<b class="pub-id">${journalId}</b>)</a>`;
+    }
+    return paperLi;
+}
+
 function addPreprints(paperData) {
     const paperList = document.querySelector('#card_preprints .paper-list');
     paperList.innerHTML = '';
@@ -72,6 +88,23 @@ function addConference(paperData, pubInfo) {
     });
 }
 
+function addJournals(paperData, pubInfo) {
+    const paperList = document.querySelector('#card_journal .paper-list');
+    paperList.innerHTML = '';
+
+    paperData.forEach(paper => {
+        const paperItem = createJournalItem(
+            paper.JOURNAL_ID,
+            paper.TITLE,
+            paper.AUTHORS,
+            paper.EQUAL_CONTRIBUTION,
+            paper.CORRESPONDENCE,
+            pubInfo[paper.JOURNAL_ID].NAME,
+            pubInfo[paper.JOURNAL_ID].LEVEL
+        );
+        paperList.appendChild(paperItem);
+    });
+}
 
 (async () => {
     const paper_files = [
@@ -90,4 +123,5 @@ function addConference(paperData, pubInfo) {
 
     addPreprints(allPapers[0].reverse());
     addConference(allPapers[1].reverse(), pubInfoDict);
+    addJournals(allPapers[2].reverse(), pubInfoDict);
 })();
